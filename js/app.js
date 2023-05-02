a4e2cff0f98872be6df153ca2c7f534bdeb57132
Renelle Tigue <titingrenelle@gmail.com>
2023-05-02 12:08:19 +0800

    Declare all needed variables on the js code

---
+++ js/app.js
@@ -0,0 +1,10 @@
+let init = () => {
+  let container = document.getElementsByClassName('jumbo-slider__container')[0];
+  let slides = document.getElementsByClassName('jumbo-slider__slide');
+  let circles = document.getElementsByClassName('jumbo-slider__circle');
+  let links = document.getElementsByClassName('jumbo-slider__link');
+  current = 1;
+  time = 6000;
+}
+
+init();
